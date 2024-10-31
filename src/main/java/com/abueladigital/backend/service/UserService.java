445042d052cc1a11edcee52ba29b85eb4e035561
Francisco Valdes <ecodisonante@gmail.com>
2024-10-31 11:51:04 -0300

    Implementacion basica de userService

--- src/main/java/com/abueladigital/backend/service/UserService.java
+++ src/main/java/com/abueladigital/backend/service/UserService.java
@@ -1,20 +1,22 @@
 package com.abueladigital.backend.service;
 
 import com.abueladigital.backend.model.User;
+
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
+
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    Optional<User> findByEmail(String email);
 
     User save(User user);
 
     User update(User user);
 
     void delete(Long id);
-
-    User findById(Long id);
-
-    List<User> findAll();
-
-    User findByEmail(String email);
 
 }
---
+++ src/main/java/com/abueladigital/backend/service/UserServiceImpl.java
@@ -0,0 +1,53 @@
+package com.abueladigital.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.abueladigital.backend.model.User;
+import com.abueladigital.backend.repository.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public User save(User user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User update(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
