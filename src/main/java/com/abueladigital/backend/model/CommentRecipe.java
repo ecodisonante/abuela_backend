5248476b399bc02167bde08b87d5b22369bc15f8
Francisco Valdes <ecodisonante@gmail.com>
2024-12-02 23:59:11 -0300

    fix: cambia nombre para evitar palabra reservada en oracle
    - Se agrega jsonIgnore en Recipe para evitar loop en get

--- src/main/java/com/abueladigital/backend/model/CommentRecipe.java
+++ src/main/java/com/abueladigital/backend/model/CommentRecipe.java
@@ -2,6 +2,8 @@ package com.abueladigital.backend.model;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -10,24 +12,26 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
-
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @NoArgsConstructor
+@Table(name = "comment_recipe") 
 public class CommentRecipe {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String comment;
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "recipe_id", nullable = true)
-    private Recipe recipe;    
+    @JsonIgnore
+    private Recipe recipe;
 
     private LocalDateTime created;
 
